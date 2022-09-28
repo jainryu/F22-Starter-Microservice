cef95cfaff21887f9238bf6ab31280602853d2f1
jainryu <jainryu0929@gmail.com>
2022-09-28 13:42:50 -0400

    initial commit

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -11,9 +11,9 @@ class ColumbiaStudentResource:
     @staticmethod
     def _get_connection():
 
-        usr = os.environ.get("DBUSER")
-        pw = os.environ.get("DBPW")
-        h = os.environ.get("DBHOST")
+        usr = "root"
+        pw = "jain0929"
+        h = "localhost"
 
         conn = pymysql.connect(
             user=usr,
@@ -27,7 +27,7 @@ class ColumbiaStudentResource:
     @staticmethod
     def get_by_key(key):
 
-        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
+        sql = "SELECT * FROM f22_databases.columbia_student where uni=%s";
         conn = ColumbiaStudentResource._get_connection()
         cur = conn.cursor()
         res = cur.execute(sql, args=key)
